199d6a4f587a5d65649bf9904ecaffdf7ac02cce
BookOwl <stanleybookowl@gmail.com>
2016-12-29 17:02:28 -0500

    Add dependencies for clipboard and clap

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
+#[macro_use]
+extern crate clap;
+extern crate clipboard;
 
 fn main() {
     println!("Hello, world!");
